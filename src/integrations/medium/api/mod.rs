0d34ea8d0dd7cf7464cf2d98c32f1433ea558292
Jon Brennecke <jpbrennecke@gmail.com>
2018-08-30 14:18:23 -0700

    formatted with rustfmt

--- src/integrations/medium/api/mod.rs
+++ src/integrations/medium/api/mod.rs
@@ -1,51 +1,48 @@
-use std::io::Read;
-use std::collections::HashMap;
-use reqwest::{self, StatusCode};
 use reqwest::header::Accept;
+use reqwest::{self, StatusCode};
 use serde_json::de::from_str;
+use std::collections::HashMap;
+use std::io::Read;
 
 use Error;
 
 #[derive(Deserialize, Debug, Clone)]
 pub struct MediumPublicationResponse {
-  pub payload: MediumPublicationResponsePayload,
+    pub payload: MediumPublicationResponsePayload,
 }
 
 #[derive(Deserialize, Debug, Clone)]
 pub struct MediumPublicationResponsePayload {
-  pub references: MediumPublicationResponsePayloadReferences,
+    pub references: MediumPublicationResponsePayloadReferences,
 }
 
 #[derive(Deserialize, Debug, Clone)]
 pub struct MediumPublicationResponsePayloadReferences {
-  #[serde(rename="Post")]
-  pub post: HashMap<String, MediumPostResponse>,
+    #[serde(rename = "Post")]
+    pub post: HashMap<String, MediumPostResponse>,
 }
 
 #[derive(Deserialize, Debug, Clone)]
 pub struct MediumPostResponse {
-  pub id: String,
-  pub title: String,
+    pub id: String,
+    pub title: String,
 }
 
 pub fn fetch_posts(publication: String) -> Result<MediumPublicationResponse, Error> {
-  let client = reqwest::Client::new();
-  let url = format!("https://medium.com/{}", publication);
-  let mut res = client
-    .get(&url)
-    .header(Accept::json())
-    .send()?;
-  match res.status() {
-    StatusCode::Ok => {
-      let mut content = String::new();
-      res.read_to_string(&mut content).unwrap();
-      let trim_content = content.trim_left_matches("])}while(1);</x>");
-      Ok(from_str::<MediumPublicationResponse>(&*trim_content)?)
-    },
-    _ => {
-      let mut content = String::new();
-      res.read_to_string(&mut content).unwrap();
-      Err(Error::StringError(content))
+    let client = reqwest::Client::new();
+    let url = format!("https://medium.com/{}", publication);
+    let mut res = client.get(&url).header(Accept::json()).send()?;
+    match res.status() {
+        StatusCode::Ok => {
+            let mut content = String::new();
+            res.read_to_string(&mut content).unwrap();
+            let trim_content = content.trim_left_matches("])}while(1);</x>");
+            Ok(from_str::<MediumPublicationResponse>(&*trim_content)?)
+        }
+        _ => {
+            let mut content = String::new();
+            res.read_to_string(&mut content).unwrap();
+            Err(Error::StringError(content))
+        }
     }
-  }
 }
--- src/main.rs
+++ src/main.rs
@@ -7,40 +7,41 @@
 // extern crate rocket_contrib;
 // extern crate rocket_cors;
 extern crate dotenv;
-#[macro_use] extern crate serde_derive;
+#[macro_use]
+extern crate serde_derive;
+extern crate argon2rs;
+extern crate chrono;
+extern crate rand;
 extern crate serde;
 extern crate serde_json;
-extern crate chrono;
-extern crate argon2rs;
-extern crate rand;
 // #[macro_use]
 // extern crate hyper;
 extern crate reqwest;
-extern crate validator;
-extern crate uuid;
 extern crate url;
+extern crate uuid;
+extern crate validator;
 
-use std::env;
-use serde_json::Error as SerdeJSONError;
 use reqwest::Error as ReqwestError;
+use serde_json::Error as SerdeJSONError;
+use std::env;
 
 #[derive(Debug)]
 pub enum Error {
-  SerdeJSONError(SerdeJSONError),
-  ReqwestError(ReqwestError),
-  StringError(String),
+    SerdeJSONError(SerdeJSONError),
+    ReqwestError(ReqwestError),
+    StringError(String),
 }
 
 impl From<SerdeJSONError> for Error {
-  fn from(error: SerdeJSONError) -> Error {
-    Error::SerdeJSONError(error)
-  }
+    fn from(error: SerdeJSONError) -> Error {
+        Error::SerdeJSONError(error)
+    }
 }
 
 impl From<ReqwestError> for Error {
-  fn from(error: ReqwestError) -> Error {
-    Error::ReqwestError(error)
-  }
+    fn from(error: ReqwestError) -> Error {
+        Error::ReqwestError(error)
+    }
 }
 
 mod integrations;
@@ -49,8 +50,9 @@ use dotenv::dotenv;
 use integrations::medium::api::fetch_posts;
 
 fn main() {
-  dotenv().ok();
-  let publication = env::var("MEDIUM_PUBLICATION").expect("Missing required parameter \"MEDIUM_PUBLICATION\"");
-  let posts = fetch_posts(publication);
-  println!("{:?}", posts);
+    dotenv().ok();
+    let publication =
+        env::var("MEDIUM_PUBLICATION").expect("Missing required parameter \"MEDIUM_PUBLICATION\"");
+    let posts = fetch_posts(publication);
+    println!("{:?}", posts);
 }
